a0b6440c07db8da8704346b28d80f75e60726a8b
Kabir on Picard <siplukabir@gmail.com>
2014-10-14 14:07:14 -0400

    Updated Sync.cpp

--- n3HeDAQ/src/core/Sync.cpp
+++ n3HeDAQ/src/core/Sync.cpp
@@ -5,37 +5,24 @@
 
 using namespace std;
 
-int DaqConfig(char *argv2)
-// int main(int argc,char *argv[])
+int Sync(bool status)
 {
-    string change_config[]={"on","off"};
-    string config;
-
+    
     Daq daq(DAQ21_IP,DAQ_PORT2,DAQ21,RUN_LENGTH);
     if(!daq.CheckStatus())
     {
-	//if(argc>1)
 	{
-	    if(argv2==change_config[0])
+	    if(status)
 	    {
-		config="do4_3 1";
 		cout<<"Trigger Enabled"<<endl;
-		daq.WriteToSocket(config.c_str());
+		daq.WriteToSocket("do4_3 1");
 	    }
-	    else if(argv2==change_config[1])
+	    else if(!status)
 	    {
-		config="do4_3 0";
 		cout<<"Trigger Disabled"<<endl;
-		daq.WriteToSocket(config.c_str());
+		daq.WriteToSocket("do4_3 0");
 	    }
 	}
-	// cout<<"Enter DAQ configuration change: "<<endl;
-	// cin>>change_config;
-        //Suggested Use
-	// daq.WriteToSocket("nacc=1,1");
-	// daq.WriteToSocket("do4_0 1");
-	// daq.WriteToSocket("set.site 1 rgm=1,0,1");
-	// daq.WriteToSocket("set.site 1 hi_res_mode=1");
     }
     return(0);
 }
@@ -44,9 +31,9 @@ int DaqConfig(char *argv2)
 // {
 //     for(int i=0;i<100;i++)
 //     {
-// 	DaqConfig("on");
+// 	Sync(true);
 // 	sleep(3);
-// 	DaqConfig("off");
+// 	Sync(false);
 // 	sleep(3);
 //     }
 //     return(0);
