eec15f5d8fc883ae54031301a7e008ad8d922795
Siplu Kabir <siplukabir@gmail.com>
2014-11-11 12:34:40 -0500

    Updated Config.cpp

--- n3HeDAQ/src/core/Config.cpp
+++ n3HeDAQ/src/core/Config.cpp
@@ -7,9 +7,20 @@
 #define SLEEP 15e5
 using namespace std;
 
-int MakeChange(int module,char *command)
+char *command=new char[100];
+
+int MakeChange(int module,char *command,int site)
 {
-    string change_config="set.site 1 ";
+    string change_config;
+    if(site==0)
+    {   
+        change_config="set.site 0 ";
+    }
+    else
+    {
+        change_config="set.site 1 ";
+    }
+
     change_config=change_config+command;
 	    
     Daq daq(DAQ30_IP,DAQ_PORT2,DAQ30,RUN_LENGTH);
@@ -19,122 +30,113 @@ int MakeChange(int module,char *command)
     }
     return(0);
 }
-int DaqConfig(unsigned int *param)
+
+//Change Frequency i.e. Sample Rate
+int ChangeRate(int module,int rate)
 {
-    char *command=new char[100];
+    //Set the external clock
+    sprintf(command,"fpmux xclk");
+    MakeChange(module,command,0);	   
 
-        //Change Frequency
-	if(param[1]!=0)
-	{
-            //Set the external clock
-	    sprintf(command,"fpmux xclk");
-	    MakeChange(21,command);	   
-	    // change_config=change_config+command;
-	    // daq.WriteToSocket(command);
-
-	    //Set the clock
-	    sprintf(command,"clk 1,1,0");
-	    MakeChange(21,command);	   
-	    // change_config=change_config+command;
-	    // daq.WriteToSocket(change_config.c_str());
+    //Set the clock
+    sprintf(command,"clk 1,1,0");
+    MakeChange(module,command,1);	   
           
-            //Divide the clock
-	    sprintf(command,"clkdiv 1");
-	    MakeChange(21,command);	   
-	    // change_config=change_config+command;
-	    // daq.WriteToSocket(change_config.c_str());
+    //Divide the clock
+    sprintf(command,"clkdiv 1");
+    MakeChange(module,command,1);	   
          
-            //Set the mb_clock
-	    sprintf(command,"mb_clk 64000 %d",param[1]);
-	    MakeChange(21,command);	   
-	    // change_config=change_config+command;
-	    // daq.WriteToSocket(command);
-	}
-
-        //Change Running Mode
-	if(param[2]!=0)
-	{
-	    if(param[2]==1)
-	    {
-		sprintf(command,"rgm=1,0,1"); //Triggered & Synchronized,d0, rising 
-	    }
-	    else if(param[2]==2)
-	    {
-		sprintf(command,"rgm=0,0,0"); //Trigger Off
-	    }
-	    else if(param[2]==3)
-	    {
-		sprintf(command,"rgm=1,0,0");//Triggered & Synchronized,d0,falling 
-	    }
-	    else if(param[2]==4)
-	    {
-		sprintf(command,"rgm=2,0,1");//Triggered & Gated,d0,Rising 
-	    }
-	    else if(param[2]==5)
-	    {
-		sprintf(command,"rgm=2,0,0");//Triggered & Gated,d0,Falling 
-	    }
-	    else if(param[2]==7)
-	    {
-		sprintf(command,"rgm=3,0,1");//Triggered & Continuous,d0,Rising 
-	    }
-	    else if(param[2]==8)
-	    {
-		sprintf(command,"rgm=3,0,0");//Triggered & Continuous,d0,Falling 
-	    }
-
-	    MakeChange(21,command);	   
-	    // change_config=change_config+command;
-	    // daq.WriteToSocket(change_config.c_str());
-	    usleep(SLEEP);
-
-          //rgm=MODE,line (0=d0, front panel), edge
-          //MODE :0=OFF, 2=RGM, 3=RTM, 1=SRTM
-          //Edge: 0=falling, 1=rising
-          //Repeating Gate Mode RGM : captures on active level of the trigger
-          //Repeating Trigger Mode RTM : starts on active edge, continues nsamples
-          //Sync RTM SRTM : resynchronises to the active edge, captures nsamples : Which is required for n3He!	   
-	}
-
-        //Change Event Length
-	if(param[3]!=0)
-	{
-	    sprintf(command,"rtm_translen=%d",param[3]);
-	    MakeChange(21,command);	   
-	    // change_config=change_config+command;
-	    // daq.WriteToSocket(change_config.c_str());
-	    usleep(SLEEP);
-	}
-
-       //Change Hi_Res_Mode (Averaging and Decimation)
-	if(param[4]!=0)
-	{
-	    sprintf(command,"hi_res_mode=%d",param[4]-1);
-	    MakeChange(21,command);	   
-	    // change_config=change_config+command;
-	    // daq.WriteToSocket(change_config.c_str());
-	    usleep(SLEEP);
-	}
-
-        //Change nacc (Averaging and Decimation)
-	if(param[5]!=0 && param[6]!=0)
-	{
-	    sprintf(command,"nacc=%d,%d",param[5],param[6]);
-	    MakeChange(21,command);	   
-	    // change_config=change_config+command;
-	    // daq.WriteToSocket(change_config.c_str());
-	    usleep(SLEEP);
-	}
-    
-    delete[] command;
+    //Set the mb_clock
+    sprintf(command,"mb_clk 64000 %d",rate);
+    MakeChange(module,command,0);	   
+   
     return(0);
 }
 
+//Change Running Mode
+int ChangeMode(int module,int mode)
+{
+
+    if(mode==1)
+    {
+	sprintf(command,"rgm=1,0,1"); //Triggered & Synchronized,d0, rising 
+    }
+    else if(mode==2)
+    {
+	sprintf(command,"rgm=0,0,0"); //Trigger Off
+    }
+    else if(mode==3)
+    {
+	sprintf(command,"rgm=1,0,0");//Triggered & Synchronized,d0,falling 
+    }
+    else if(mode==4)
+    {
+	sprintf(command,"rgm=2,0,1");//Triggered & Gated,d0,Rising 
+    }
+    else if(mode==5)
+    {
+	sprintf(command,"rgm=2,0,0");//Triggered & Gated,d0,Falling 
+    }
+    else if(mode==7)
+    {
+	sprintf(command,"rgm=3,0,1");//Triggered & Continuous,d0,Rising 
+    }
+    else if(mode==8)
+    {
+	sprintf(command,"rgm=3,0,0");//Triggered & Continuous,d0,Falling 
+    }
+
+    MakeChange(module,command,1);	   
+    return(0);
+    //rgm=MODE,line (0=d0, front panel), edge
+    //MODE :0=OFF, 2=RGM, 3=RTM, 1=SRTM
+    //Edge: 0=falling, 1=rising
+    //Repeating Gate Mode RGM : captures on active level of the trigger
+    //Repeating Trigger Mode RTM : starts on active edge, continues nsamples
+    //Sync RTM SRTM : resynchronises to the active edge, captures nsamples : Which is required for n3He!	   
+}
+
+//Change Event Length
+int ChangeLength(int module,int length)
+{
+    //Change Event Length
+    sprintf(command,"rtm_translen=%d",length);
+    MakeChange(module,command,1);	   
+
+    return(0);
+}
+
+
+//Change Hi_Res_Mode
+int ChangeRes(int module,int res)
+{
+    sprintf(command,"hi_res_mode=%d",res);
+    MakeChange(module,command,1);	   
+    return(0);
+}
+
+//Change nacc (Averaging)
+int ChangeAveraging(int module,int avg)
+{
+    sprintf(command,"nacc=%d,%d",avg,1);
+    MakeChange(module,command,1);	   
+    return(0);
+}
+
+//Change nacc (Decimation)
+int ChangeDecimation(int module,int dec)
+{
+    sprintf(command,"nacc=%d,%d",dec,dec);
+    MakeChange(module,command,1);	   
+    return(0);
+}    
+    
 int ChangeConfig(void)
 // int main(void)
 {
-    unsigned int *param=new unsigned int[7];
-
+    unsigned int param;
+    unsigned int value;
+    unsigned int module;
 //cout<<"Select Options"<<endl;
 //cout<<"1:To set n3he default DAQ setting for all the modules"<<endl;
 //cout<<"2:To set n3he default DAQ setting for one modules"<<endl;
@@ -142,62 +144,85 @@ int ChangeConfig(void)
 //cout<<"4:To Print current DAQ setting"<<endl; 
 
     cout<<"Enter Config in the following format. Enter Zero in a field if you do not want to that parameter. "<<endl;    
-    cout<<"MODULE RATE MODE EVENT_LENGTH HI_RES_MODE AVERAGING DECIMATION"<<endl;
-    cin>>param[0];  //DAQ Module
-    cin>>param[1];  //Sample Rate
-    cin>>param[2];  //Running Moade
-    cin>>param[3];  //Event Length
-    cin>>param[4];  //Hi_Res_Mode
-    cin>>param[5];  //Averaging
-    cin>>param[6];  //Decimation
+    cout<<"1:RATE 2:MODE 3:EVENT_LENGTH 4:HI_RES_MODE 5:AVERAGING 6:DECIMATION"<<endl;
+    cout<<"Enter Module:"<<endl;
+    cin>>module;
+    cout<<"Enter Parameter Code followed by it's value:"<<endl;
+    cin>>param;  //DAQ Module
+    cin>>value;  //Sample Rate
+    // cin>>param[2];  //Running Moade
+    // cin>>param[3];  //Event Length
+    // cin>>param[4];  //Hi_Res_Mode
+    // cin>>param[5];  //Averaging
+    // cin>>param[6];  //Decimation
     
-    cout<<"You entered: \nModule:"<<param[0]<<"  \nRate:"<<param[1]<<" \nMode:"<<param[2]<<" \nEvent Length:"<<param[3]<<"  \nHi Res Mode:"<<param[4]<<"  \nAveraging:"<<param[5]<<"  \nDecimation:"<<param[6]<<endl;
+    // cout<<"You entered: \nModule:"<<param[0]<<"  \nRate:"<<param[1]<<" \nMode:"<<param[2]<<" \nEvent Length:"<<param[3]<<"  \nHi Res Mode:"<<param[4]<<"  \nAveraging:"<<param[5]<<"  \nDecimation:"<<param[6]<<endl;
 
-    if(!(param[0]==21 ||  param[0]==22 || param[0]==23 || param[0]==24 ||param[0]==26 || param[0]==30))
+    if(!(module==21 ||  module==22 || module==23 || module==24 ||module==26 || module==30))
     {
 	cout<<"Invalid Module"<<endl;
         return(1);
     }
-    if(param[1]<0 || param[1] >128)
+    if(param==1)
     {
-	cout<<"Invalid Rate. It should be between 1-128 KHz"<<endl;
-        return(1);
+	if(value<0 || value >128)
+	{
+	    cout<<"Invalid Rate. It should be between 1-128 KHz"<<endl;
+	    return(1);
+	}
+	else
+	    ChangeRate(module,value);
     }
-    if(param[2]<0 || param[2] >8) 
+    if(param==2)
     {
-	cout<<"Invalid mode. Check Syntax"<<endl;
-        return(1);
+	if(value<0 || value >8) 
+	{
+	    cout<<"Invalid mode. Check Syntax"<<endl;
+	    return(1);
+	}
+	else
+	    ChangeMode(module,value);
     }
-    if(param[3]<39 && param[3]!=0)
+    if(param==3)
     {
-	cout<<"Invalid Event Length.Must be greater than 39."<<endl;
-        return(1);
+	if(value<39 && value!=0)
+	{
+	    cout<<"Invalid Event Length.Must be greater than 39."<<endl;
+	    return(1);
+	}
+	else
+	    ChangeLength(module,value);
     }
-    if(param[4] <0 || param[4] >2)
+    if(param==4)
     {
-	cout<<"Invalid Hi_Res_Mode"<<endl;
-        return(1);
+	if(value <0 || value >2)
+	{
+	    cout<<"Invalid Hi_Res_Mode"<<endl;
+	    return(1);
+	}
+	else
+	    ChangeRes(module,value);
     }
-    if(param[5] < 0 || param[5] > 64 )
+    if(param==5)
     {
-	cout<<"Invalid Averaging."<<endl;
-        return(1);
+	if(value < 0 || value > 64 )
+	{
+	    cout<<"Invalid Averaging."<<endl;
+	    return(1);
+	}
+	else
+	    ChangeAveraging(module,value);
     }
-    if((param[6]!=1 && param[6]!=param[6] && param[6]!=0)|| ((param[6] & (param[6] - 1))) || param[6]<0)
+    if(param==6)
     {
-        cout <<"Invalid Decimation.Note it should be power of 2." <<endl; 
-        return(1);
+	if((value!=1 && value!=value && value!=0)|| ((value & (value - 1))) || value<0)
+	{
+	    cout <<"Invalid Decimation.Note it should be power of 2." <<endl; 
+	    return(1);
+	}
+	else
+	    ChangeDecimation(module,value);
     }
-    DaqConfig(param);
     cout<<"Done with the changes !!!"<<endl;
-    delete[] param;
     return 0;
 }
-
-//Suggested demo use:
-        //daq.WriteToSocket(command);
-        //Suggested Use
-    	// daq.WriteToSocket("nacc=1,1");
-    	// daq.WriteToSocket("do4_0 1");
-    	// daq.WriteToSocket("set.site 1 rgm=1,0,1");
-    	// daq.WriteToSocket("set.site 1 hi_res_mode=1");
