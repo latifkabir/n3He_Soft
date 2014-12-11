06a6a4eebcf4051c04389163ce4877942292aea6
Siplu Kabir <siplukabir@gmail.com>
2014-12-11 17:53:00 -0500

    Updated Progress Bar script to give dynamic (variable length) progress bar

--- n3HeDAQ/src/core/Progress.cpp
+++ n3HeDAQ/src/core/Progress.cpp
@@ -1,35 +1,52 @@
 //Program to show progress status bar of current run
 //Author:Latiful Kabir
 //Date: 9.29.14
-
-#include<iostream>
-#include<unistd.h>
-using namespace std;
-
-void printProgBar( int percent )
-{
-    std::string bar;
-
-    for(int i = 0; i < 50; i++)
-    {
-	if( i < (percent/2))
-	{
-	    bar.replace(i,1,"=");
+#include <iostream> 
+#include <string> 
+#include <unistd.h> 
+#include <ncurses.h> 
+#include <sys/ioctl.h> 
+#include <stdlib.h> 
+       
+using namespace std; 
+       
+void printProgBar(int percent)      // percent -> Percentage 
+{ 
+    string bar; 
+    struct winsize uk; 
+    if(ioctl(0,TIOCGWINSZ,&uk) != 0) 
+    { 
+	exit(1); 
+    } 
+    int wdt = uk.ws_col - 50; // create some space for the other text 
+    if(wdt < 5) 
+    { 
+	wdt = 5;      // minimum width for progress bar 5 char 
+    } 
+       
+    for(int i=0; i < wdt; i++) 
+    { 
+	if(i < (percent*wdt/100)) 
+	{ 
+	    bar.replace(i,1,"="); 
+	} 
+	else if(i == (percent*wdt/100))
+	{ 
+	    bar.replace(i,1,">"); 
 	}
-	else if( i == (percent/2))
-	{
-	    bar.replace(i,1,">");
-	}
-	else
-	{
-	    bar.replace(i,1," ");
-	}
-    }
+	else 
+	{ 
+	    bar.replace(i,1," "); 
+	} 
+    } 
+    cout << "\r";          // go to the first character in terminal 
+    cout.width(3);          // set some space for percentage 
+    cout<<"\t\t";
+    cout << percent << "%";      
+    cout << "[" << bar << "] ";     // progress bar        
+    cout<< flush; 
+} 
 
-    std::cout<< "\r" "\t\t\t[" << bar << "] ";
-    std::cout.width( 3 );
-    std::cout<< percent << "%     " << std::flush;
-}
 
 /*
 //Demo usage:
