5694413d84857e0c295fe34d5fc92c856d4d80c1
Kabir on Picard <siplukabir@gmail.com>
2014-12-16 17:21:35 -0500

    Done with 1cm spacing Upstream scan

--- n3HeScan/beam/src/xy/ConstantsXY.h
+++ n3HeScan/beam/src/xy/ConstantsXY.h
@@ -2,30 +2,45 @@
 #define BAUD 9600 
 
 //Test Scan for Program
-#define UNIT_X 5000
-#define UNIT_Y 500 
-#define MAX_X 5e4
-#define MAX_Y 5e4
-#define START_X 1e4
-#define END_X 2.5e4
-#define START_Y 1e3
-#define END_Y 2.0e3
+//#define UNIT_X 5000
+//#define UNIT_Y 500 
+//#define MAX_X 5e4
+//#define MAX_Y 5e4
+//#define START_X 1e4
+//#define END_X 2.5e4
+//#define START_Y 1e3
+//#define END_Y 2.0e3
+//#define CHANNEL_M4 2
+//#define CHANNEL_M1 0
+//#define DIRTY_DAQ 30
+
+//Upstream 17x19 grid(1cm spacing) scan
+#define UNIT_X 1575
+#define UNIT_Y 1575
+#define MAX_X 10e4
+#define MAX_Y 10e4
+#define START_X 3192
+#define END_X 28392
+#define START_Y 4786
+#define END_Y 33136
 #define CHANNEL_M4 2
 #define CHANNEL_M1 0
 #define DIRTY_DAQ 30
 
-//Upstream 9x10 grid(2cm spacing) scan
-//#define UNIT_X 3150
-//#define UNIT_Y 3150 
-//#define MAX_X 5e4
-//#define MAX_Y 5e4
-//#define START_X 2285
-//#define END_X 30633
-//#define START_Y 3752
-//#define END_Y 35248
-//#define CHANNEL_M4 2
-//#define CHANNEL_M1 0
-//#define DIRTY_DAQ 30
+
+//Upstream 17x19 grid(1cm spacing) scan
+/* #define UNIT_X 1575 */
+/* #define UNIT_Y 1575  */
+/* #define MAX_X 10e4 */
+/* #define MAX_Y 10e4 */
+/* #define START_X 3192 */
+/* #define END_X 28392 */
+/* #define START_Y 18961 */
+/* #define END_Y 18961 */
+/* #define CHANNEL_M4 2 */
+/* #define CHANNEL_M1 0 */
+/* #define DIRTY_DAQ 30 */
+
 
 
 //Downstream 12x13 grid(1.5cm spacing) scan
